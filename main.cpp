21eb8af9fd5f1f77c2765381bdd79181be395aab
Peter <pepo.ghali@yahoo.com>
2023-11-02 18:01:08 +0200

    added getMin function

--- main.cpp
+++ main.cpp
@@ -11,13 +11,24 @@ int getSum(vector<int> &nums) {
     return sum;
 }
 
+int getMin(vector<int> &nums) {
+    int min = INT_MAX;
+    for (int num : nums) {
+        if (num < min) {
+            min = num;
+        }
+    }
+    return min;
+}
 
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int sum = getSum(nums);
-
     cout << sum << endl;
+
+    int min = getMin(nums);
+    cout << min << endl;
 
     return 0;
 }
